3789a3c162b6109bc591b7bfcaea2049fb441f0f
Omar Ehab Santawy <111521164+Santos1012@users.noreply.github.com>
2024-06-24 19:53:38 +0300

    edit add item ui

--- lib/features/current_reservation/presentation/views/widgets/add_items_body.dart
+++ lib/features/current_reservation/presentation/views/widgets/add_items_body.dart
@@ -3,13 +3,21 @@ import 'dart:developer';
 import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:saghaf_desktop/core/utils/media_query.dart';
-import 'package:saghaf_desktop/core/widgets/app_custom_text_field.dart';
+import 'package:saghaf_desktop/features/current_reservation/presentation/views/widgets/items_custom_text_field.dart';
 
-class AddItemsBody extends StatelessWidget {
+class AddItemsBody extends StatefulWidget {
   final void Function()? onTap;
 
   const AddItemsBody({super.key, this.onTap});
 
+  @override
+  State<AddItemsBody> createState() => _AddItemsBodyState();
+}
+
+class _AddItemsBodyState extends State<AddItemsBody> {
+  int itemCountText = 1;
+  int itemPriceText = 20;
+  int itemTotalPriceText = 20;
   @override
   Widget build(BuildContext context) {
     final List<String> items = ['tea', 'coffee', 'water', 'milk'];
@@ -19,35 +27,29 @@ class AddItemsBody extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Add Item",
-                    style: TextStyle(
-                      fontSize: 24.w(context),
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "Comfortaa",
-                    )),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(5),
-                      ),
-                      border: Border.all(
-                        color: const Color(0xFF20473E).withOpacity(0.75),
-                      ),
-                      shape: BoxShape.rectangle),
-                  child: Icon(
-                    Icons.add,
-                    size: 50.h(context),
-                    color: const Color(0xFFF04C29),
-                  ),
-                )
+                // Container(
+                //   decoration: BoxDecoration(
+                //       borderRadius: const BorderRadius.all(
+                //         Radius.circular(5),
+                //       ),
+                //       border: Border.all(
+                //         color: const Color(0xFF20473E).withOpacity(0.75),
+                //       ),
+                //       shape: BoxShape.rectangle),
+                //   child: Icon(
+                //     Icons.add,
+                //     size: 50.h(context),
+                //     color: const Color(0xFFF04C29),
+                //   ),
+                // )
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 135,
@@ -56,82 +58,121 @@ class AddItemsBody extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Expanded(
-                          child: CustomDropdown<String>(
-                        hintText: 'add item',
-                        headerBuilder: (context, selectedItem) {
-                          return Text(
-                            selectedItem,
-                          );
-                        },
-                        items: items,
-                        initialItem: items[0],
-                        decoration: CustomDropdownDecoration(
-                          closedBorder: Border.all(
-                              color: const Color(0xFFB1B1B1), width: 1),
-                          expandedBorder: Border.all(
-                              color: const Color(0xFFB1B1B1), width: 1),
-                        ),
-                        onChanged: (value) {
-                          log('changing value to: $value');
-                        },
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Add Item",
+                              style: TextStyle(
+                                fontSize: 24.w(context),
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "Comfortaa",
+                              )),
+                          SizedBox(
+                            height: 32.h(context),
+                          ),
+                          CustomDropdown<String>(
+                            hintText: 'add item',
+                            headerBuilder: (context, selectedItem) {
+                              return Text(
+                                selectedItem,
+                              );
+                            },
+                            items: items,
+                            initialItem: items[0],
+                            decoration: CustomDropdownDecoration(
+                              closedBorder: Border.all(
+                                  color: const Color(0xFFB1B1B1), width: 1),
+                              expandedBorder: Border.all(
+                                  color: const Color(0xFFB1B1B1), width: 1),
+                            ),
+                            onChanged: (value) {
+                              log('changing value to: $value');
+                            },
+                          ),
+                        ],
                       )),
                       SizedBoxApp(
                         w: 25.w(context),
                       ),
                       Expanded(
-                          child: AppCustomTextField(
-                        hint: "20 LE",
-                        readOnly: false,
-                        textName: "Price",
-                        controller: TextEditingController(),
-                        color: Colors.white,
-                      ))
+                        child: ItemsCustomTextField(
+                          hint:
+                              "count:  ${itemCountText.toString()}   price:   ${itemPriceText.toString()}   Total:   ${itemTotalPriceText.toString()} LE",
+                          textName: "Price",
+                          controller: TextEditingController(),
+                          color: Colors.white,
+                          maxCount: 20,
+                          count: itemCountText,
+                          onAddTap: () {
+                            if (itemCountText < 20) {
+                              ++itemCountText;
+                              itemTotalPriceText =
+                                  itemCountText * itemPriceText;
+                              setState(() {});
+                            }
+                          },
+                          onRemoveTap: () {
+                            if (itemCountText > 0) {
+                              --itemCountText;
+                              itemTotalPriceText =
+                                  itemCountText * itemPriceText;
+                              setState(() {});
+                            }
+                          },
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(5),
-                      ),
-                      border: Border.all(
-                        color: const Color(0xFF20473E).withOpacity(0.75),
-                      ),
-                      shape: BoxShape.rectangle,
-                    ),
-                    child: Center(
-                      child: Icon(
-                        Icons.remove,
-                        size: 50.h(context),
-                        color: const Color(0xFFF04C29),
-                      ),
-                    ),
-                  ),
-                )
+                // GestureDetector(
+                //   onTap: onTap,
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //       borderRadius: const BorderRadius.all(
+                //         Radius.circular(5),
+                //       ),
+                //       border: Border.all(
+                //         color: const Color(0xFF20473E).withOpacity(0.75),
+                //       ),
+                //       shape: BoxShape.rectangle,
+                //     ),
+                //     child: Center(
+                //       child: Icon(
+                //         Icons.remove,
+                //         size: 50.h(context),
+                //         color: const Color(0xFFF04C29),
+                //       ),
+                //     ),
+                //   ),
+                // )
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 GestureDetector(
-                  onTap: onTap,
+                  onTap: widget.onTap,
                   child: Container(
-                      width: 175.w(context),
-                      height: 50.h(context),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF20473E).withOpacity(0.75),
-                        borderRadius: BorderRadius.circular(10),
+                    width: 175.w(context),
+                    height: 50.h(context),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF20473E).withOpacity(0.75),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Done",
+                        style: TextStyle(
+                          fontSize: 20.w(context),
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Comfortaa",
+                        ),
                       ),
-                      child: Center(
-                          child: Text("Done",
-                              style: TextStyle(
-                                fontSize: 20.w(context),
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontFamily: "Comfortaa",
-                              )))),
+                    ),
+                  ),
                 )
               ],
             ),
---
+++ lib/features/current_reservation/presentation/views/widgets/items_custom_text_field.dart
@@ -0,0 +1,109 @@
+import 'package:saghaf_desktop/core/utils/imports.dart';
+import 'package:saghaf_desktop/core/utils/media_query.dart';
+
+class ItemsCustomTextField extends StatelessWidget {
+  final String hint;
+  final bool? isSearch;
+  final String textName;
+  final TextEditingController? controller;
+  final bool? readOnly;
+  final void Function()? onRemoveTap;
+  final void Function()? onAddTap;
+  final Color? color;
+  final bool? phone;
+  final int maxCount;
+  final int count;
+  final IconData? icon;
+
+  const ItemsCustomTextField(
+      {super.key,
+      required this.hint,
+      required this.maxCount,
+      required this.count,
+      this.isSearch = false,
+      required this.textName,
+      this.controller,
+      this.readOnly,
+      this.onRemoveTap,
+      this.onAddTap,
+      this.color,
+      this.phone = false,
+      this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          textName,
+          style: TextStyle(
+              fontSize: 24.w(context),
+              fontWeight: FontWeight.w500,
+              fontFamily: "Comfortaa"),
+        ),
+        SizedBox(
+          height: 32.h(context),
+        ),
+        TextFormField(
+          controller: controller,
+          readOnly: true,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: 20.w(context), vertical: 20.h(context)),
+            hintText: hint,
+            hintStyle: const TextStyle(fontFamily: "Comfortaa"),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color:
+                      isSearch == true ? Colors.grey : const Color(0xFFA4A4A4)),
+              borderRadius: BorderRadius.circular(14.w(context)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: isSearch == true
+                      ? Colors.grey.withOpacity(0.2)
+                      : const Color(0xFFA4A4A4)),
+              borderRadius: BorderRadius.circular(14.w(context)),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: isSearch == true
+                      ? Colors.grey.withOpacity(0.2)
+                      : const Color(0xFFA4A4A4)),
+              borderRadius: BorderRadius.circular(14.w(context)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: isSearch == true
+                      ? Colors.grey.withOpacity(0.2)
+                      : const Color(0xFFA4A4A4)),
+              borderRadius: BorderRadius.circular(14.w(context)),
+            ),
+            prefixIcon: GestureDetector(
+              onTap: onRemoveTap,
+              child: Icon(
+                Icons.remove,
+                color: count >0
+                    ? const Color(0xFF20473E).withOpacity(0.75)
+                    : Colors.grey,
+              ),
+            ),
+            
+            fillColor: color ?? Colors.grey,
+            filled: true,
+            suffixIcon: GestureDetector(
+              onTap: onAddTap,
+              child: Icon(
+                Icons.add,
+                color: count < maxCount
+                    ? const Color(0xFF20473E).withOpacity(0.75)
+                    : Colors.grey,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
